948044b47bc616b426ddb13ecea1a272d9afc16d
quangtam-hust-99 <75573618+quangtam-hust-99@users.noreply.github.com>
2022-04-19 10:52:25 +0700

    Add files via upload

--- tb_i2c.sv
+++ tb_i2c.sv
@@ -1,9 +1,10 @@
+`timescale 1ns / 1ps
 module tb_i2c;
 
 reg clk ,rst;
 
 reg [6:0] addr;
-reg [7:0] data;//data2,data3,data4;
+reg [7:0] data0,data1,data2,data3,data4;
 reg rw;
 wire sda,scl;
 /*
@@ -25,10 +26,18 @@ initial begin
     #10 rst =1;
    addr = 7'b001_0000;
    rw =0;
-   data = 8'b0000_0001;
-  /* data2 = 8'b0000_0010;
-   data3 = 8'b0000_0100;
-   data4 = 8'b0000_1000;*/
-    forever #5 clk =~clk;
+    data4 = 8'b0000_0000; // byte data ??u ti�n l� byte ??a ch? n�n ph?i truy?n n+1 byte data
+   data1 = 8'b0000_1000;
+   data2 = 8'b0000_0100;
+   data3 = 8'b0000_0010;
+   data0 = 8'b0000_0001;
+   # 285000 rw =0;
+    data4 = 8'b1111_1100;
+    # 50000 rw =1;
+
+
+
+
 end
+always begin #5 clk =~clk; end
 endmodule
