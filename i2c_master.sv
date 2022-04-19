281a77cb139d62f23e90fb5d85fcee82ee3206e5
quangtam-hust-99 <75573618+quangtam-hust-99@users.noreply.github.com>
2022-04-19 10:51:58 +0700

    Add files via upload

--- i2c_master.sv
+++ i2c_master.sv
@@ -11,13 +11,13 @@ module i2c_master(
 reg[3:0] Q,Q_next;
 reg [3:0] counter, counter_byte,counter_data_read;
 wire [7:0] mem_in[0:4]; // tranfer data from master to slave
-reg [7:0] mem_out [0:3]; // tranfer data from master to slave
+reg [7:0] mem_out [0:4]; // tranfer data from master to slave
 wire sclk,clk_en;
 reg sda_o;
 reg scl_en,scl_idle,sta_sto;
 reg i2c_done =0;
 reg ready = 0;
-reg [7:0] data_out = 0 ;
+//reg [7:0] data_out = 0 ;
 reg [7:0] data_addr_rw ;
 reg data_read_inval;
 reg [3:0] cnt;
@@ -30,7 +30,7 @@ localparam  [3:0]
                     WRITE_DATA = 5,     // data from master to slave
                     READ_ACK_DATA = 6,  // ACK data from master to slave
                     READ_DATA = 7,      // data from slave to master
-                    WRITE_ACK = 8,      // ACK data from slave to master
+                    WRITE_ACK_DATA = 8,      // ACK data from slave to master
                     SR = 9,             // repeat start
                     STOP = 10;
 div_clk #(.m(249),.n(10)) uut(
@@ -124,21 +124,23 @@ always_comb
                         end   
             READ_DATA ://7
                         begin 
-                            mem_out[counter_data_read][counter] = sda;
+                            mem_out[counter_byte][counter] = sda;
                             if(counter==0)
                                 begin
-                                    Q_next = WRITE_ACK;
+                                    Q_next = WRITE_ACK_DATA;
                                 end
                         end
-            WRITE_ACK ://8
+            WRITE_ACK_DATA ://8
                         begin 
-                            if(counter_data_read == 0)
+                            if(counter_byte == 0)
                             begin
                                 Q_next = STOP;
                                 sda_o = 1'b0;
                             end
                             else 
+                            begin
                                 Q_next = READ_DATA;
+                            end
                         end
             STOP : //9
                         begin 
@@ -156,8 +158,6 @@ always_ff @(negedge sclk)
                     START :begin 
                                     counter <= 7;
                                     counter_byte <=0;
-                                    counter_data_read <=0;
-                                    data_read_inval <=0;
                                 end
                     ADDR : 
                                 begin 
@@ -167,8 +167,6 @@ always_ff @(negedge sclk)
                                 begin
                                      counter <= 7;
                                      counter_byte <= 4;
-                                     counter_data_read <= 4;
-                                     cnt <= 0;
                                  end
                     WRITE_DATA :
                                 begin 
@@ -177,15 +175,14 @@ always_ff @(negedge sclk)
                     READ_DATA:
                                 begin 
                                     counter <= counter -1;
-                                    cnt <= cnt +1;
-                                    if(cnt==7)
-                                        data_read_inval <= 1;
-                                    else 
-                                        data_read_inval <= 0;
                                 end
-                    WRITE_ACK : begin
-                                    counter_data_read <= counter_data_read -1;
+                    WRITE_ACK_DATA :
+                                begin
                                     counter <= 7;
+                                    if((counter_byte > 4) || (counter < 0))
+                                    counter_byte <= 4;
+                                    else
+                                    counter_byte <= counter_byte -1;
                                 end
                     READ_ACK_DATA : 
                                 begin
@@ -233,7 +230,7 @@ begin
                 begin 
                     scl_en = sta_sto; 
                 end
-        WRITE_ACK : 
+        WRITE_ACK_DATA : 
                 begin 
                     scl_en = sta_sto; 
                 end
