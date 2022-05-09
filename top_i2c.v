b9b54d11da039843a372eee40323d0ee0f9edadb
quangtam-hust-99 <75573618+quangtam-hust-99@users.noreply.github.com>
2022-05-09 11:49:40 +0700

    Add files via upload

--- top_i2c.v
+++ top_i2c.v
@@ -2,20 +2,24 @@ module top_i2c
 ( 
     input   clk,
     input   rst,
-    input   [6:0] addr,
-    input   [7:0] data,
-    input rw,
-    inout  sda,
-    inout  scl
+    input   wire [3:0] data_cnt  ,
+    input        [6:0] addr      ,
+    input        [7:0] data_in   ,
+    input              rw        ,
+    inout              sda       ,
+    inout              scl       ,
+    output             i_txff_rd
 );
 i2c_master master_i2c(
     .clk(clk),
     .rst(rst),
     .addr(addr),
     .rw(rw),
-    .data(data),
+    .data_cnt(data_cnt),
+    .data_in(data_in),
     .sda(sda),
-    .scl(scl)
+    .scl(scl),
+    .i_txff_rd(i_txff_rd)
 );
 
 i2c_slave_model salve_i2c(
